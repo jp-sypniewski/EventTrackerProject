cdc8731772779ba2d25188f2d99a3170a2b20e2e
jp-sypniewski <jpsypniewski@icloud.com>
2020-02-07 14:01:13 -0700

    deleted and created new boot to rename

--- EventTrackerRest/src/main/java/com/skilldistillery/eventtracker/EventTrackerRestApplication.java
+++ EventTrackerRest/src/main/java/com/skilldistillery/eventtracker/EventTrackerRestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventTrackerApplication {
+public class EventTrackerRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventTrackerApplication.class, args);
+		SpringApplication.run(EventTrackerRestApplication.class, args);
 	}
 
 }
