071fb6910e1fd016a9d87dd41d6ee17ab2e89bcf
jp-sypniewski <jpsypniewski@icloud.com>
2020-02-07 13:44:47 -0700

    update fields, add get/set constructors tostring hashcode equals to task

--- JPAEventTracker/src/main/java/com/skilldistillery/jpaeventtracker/entities/Status.java
+++ JPAEventTracker/src/main/java/com/skilldistillery/jpaeventtracker/entities/Status.java
@@ -1,5 +1,5 @@
 package com.skilldistillery.jpaeventtracker.entities;
 
 public enum Status {
-
+	open, in_progress, abandoned, completed;
 }
--- JPAEventTracker/src/main/java/com/skilldistillery/jpaeventtracker/entities/Task.java
+++ JPAEventTracker/src/main/java/com/skilldistillery/jpaeventtracker/entities/Task.java
@@ -2,21 +2,128 @@ package com.skilldistillery.jpaeventtracker.entities;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 public class Task {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	private String name;
 	
+	@Column(name="created_at")
+	@CreationTimestamp
 	private LocalDateTime createdAt;
 	
+	@Column(name="updated_at")
+	@UpdateTimestamp
 	private LocalDateTime updatedAt;
 	
+	@Column(name = "due_date")
 	private LocalDateTime dueDate;
 	
+	@Enumerated(EnumType.STRING)
 	private Status status;
+
+	public Task() {
+		super();
+	}
+
+	public Task(int id, String name, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime dueDate,
+			Status status) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.dueDate = dueDate;
+		this.status = status;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public LocalDateTime getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDateTime dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Task [id=" + id + ", name=" + name + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt
+				+ ", dueDate=" + dueDate + ", status=" + status + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 
 }
