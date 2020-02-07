d8320f1efc9fc7fc7b363b52f5d7904b37f447a2
jp-sypniewski <jpsypniewski@icloud.com>
2020-02-07 14:30:01 -0700

    add methods for get all, get one, update one, create one, delete one to service and service impl

--- EventTrackerRest/src/main/java/com/skilldistillery/eventtracker/services/TaskService.java
+++ EventTrackerRest/src/main/java/com/skilldistillery/eventtracker/services/TaskService.java
@@ -1,5 +1,17 @@
 package com.skilldistillery.eventtracker.services;
 
+import java.util.List;
+
+import com.skilldistillery.eventtracker.entities.Task;
+
 public interface TaskService {
+
+	List<Task> getAllTasks();
+
+	Task getTaskById(int id);
+
+	Task updateTask(Task task);
+
+	Task createTask(Task task);
 
 }
--- EventTrackerRest/src/main/java/com/skilldistillery/eventtracker/services/TaskServiceImpl.java
+++ EventTrackerRest/src/main/java/com/skilldistillery/eventtracker/services/TaskServiceImpl.java
@@ -1,8 +1,58 @@
 package com.skilldistillery.eventtracker.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.eventtracker.entities.Task;
+import com.skilldistillery.eventtracker.repositories.TaskRepository;
 
 @Service
 public class TaskServiceImpl implements TaskService {
+	
+	@Autowired
+	private TaskRepository repo;
+	
+	@Override
+	public List<Task> getAllTasks(){
+		return repo.findAll();
+	}
+	
+	@Override
+	public Task getTaskById(int id) {
+		Optional<Task> opt = repo.findById(id);
+		Task task = null;
+		if (opt.isPresent()) {
+			task = opt.get();
+		}
+		return task;
+	}
+	
+	@Override
+	public Task createTask(Task task) {
+		task = repo.saveAndFlush(task);
+		return task;
+	}
+	
+	@Override
+	public Task updateTask(Task task) {
+		Task updatedTask = new Task();
+		updatedTask.setId(task.getId());
+		updatedTask.setName(task.getName());
+		updatedTask.setStatus(task.getStatus());
+		updatedTask.setDueDate(task.getDueDate());
+		updatedTask = repo.saveAndFlush(updatedTask);
+		return updatedTask;
+	}
+	
+	public boolean deleteTask(int id) {
+		boolean deleted = false;
+		
+		
+		
+		return deleted;
+	}
 
 }
