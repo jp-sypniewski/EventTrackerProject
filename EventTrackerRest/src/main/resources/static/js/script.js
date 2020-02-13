bee618664bacdf2e84765601cb3b30eb13bfe8f0
jp-sypniewski <jpsypniewski@icloud.com>
2020-02-12 22:06:14 -0700

    add form for nex task to index.html, refactor script to init table instead of ul

--- EventTrackerRest/src/main/resources/static/js/script.js
+++ EventTrackerRest/src/main/resources/static/js/script.js
@@ -1,32 +1,53 @@
 window.addEventListener('load', function(){
-  console.log("window event listener loaded");
-
   var xhr = new XMLHttpRequest();
-
   xhr.open('GET', 'api/teams/1/tasks', true);
-
   xhr.onreadystatechange = function() {
-
+//	  api returns good
   	if (xhr.readyState === 4 && xhr.status < 400) {
   		var data = JSON.parse(xhr.responseText);
+  		// get dedicated <div> for initialize
   		var initDiv = document.getElementById('init');
-  		var ulTag = document.createElement('ul');
-  		for (let i = 0; i < data.length; i++){
-  			var liTag = document.createElement('li');
-  			liTag.textContent = data[i].name;
-  			ulTag.appendChild(liTag);
-  		}
-  		initDiv.appendChild(ulTag);
-  		console.log(data);
+  		var table = document.createElement('table');
   		
+  		var tableHead = document.createElement('thead');
+  		var headRow = document.createElement('tr');
+  		
+  		var headName = document.createElement('th');
+  		headName.textContent = "NAME";
+  		headRow.appendChild(headName);
+  		
+  		
+  		
+  		
+  		var tableBody = document.createElement('tbody');
+  		
+  		for (let i = 0; i < data.length; i++){
+  			// create <tr> for each task with <td> for data
+  			var tableRow = document.createElement('tr');
+  			
+  			var rowName = document.createElement('td');
+  			rowName.textContent = data[i].name;
+  			tableRow.appendChild(rowName);
+  			
+  			var rowStatus = document.createElement('td');
+  			rowStatus.textContent = data[i].status;
+  			tableRow.appendChild(rowStatus)
+  			
+  			var rowDue = document.createElement('td');
+  			rowDue.textContent = data[i].dueDate;
+  			tableRow.appendChild(rowDue);
+  			
+  			table.appendChild(tableRow);
+  		}
+  		table.appendChild(tableBody);
+  		initDiv.appendChild(table);
+  		console.log(data);	
   	}
-
+//  	api returns bad
   	if (xhr.readyState === 4 && xhr.status >= 400) {
   		console.error(xhr.status + ': ' + xhr.responseText);
-  	}
-  	
+  	}	
   };
-
   xhr.send(null);
-
 });
+
