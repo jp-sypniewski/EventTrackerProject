81fb19d14e5844eb6db21f2625c61a1063cfd34a
jp-sypniewski <jpsypniewski@icloud.com>
2020-02-19 10:23:40 -0700

    stub components, models, services for angular

---
+++ ngEvents/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskComponent } from './components/task/task.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ngEvents/src/app/models/task.ts
@@ -0,0 +1,11 @@
+export class Task {
+
+  id: number;
+  name: string;
+  status: string;
+  dueDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+
+
+}
